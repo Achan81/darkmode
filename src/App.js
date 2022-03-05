c6051d0564c5e5a9495fc5474807e1707d7babc8
achan81 <achan81@icloud.com>
2022-03-05 10:12:31 +0000

    test complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-function App() {
-  return <h1>Hello World</h1>
-}
+import React from 'react'
 
+import Home from './components/common/Home'
+import About from './components/common/About'
+
+
+function App() {
+  return ( 
+    <>
+
+      <div className="container-lg">
+        <div className="row justify-content-center">
+          <div className="mainapp col-11">
+            <Home/>
+            <About/>
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
 export default App
---
+++ src/components/common/About.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function About() {
+  return (
+
+    <div className="container-fluid justify-content-center blurry-text text-uppercase opacity-100" id="about">
+      <h1 className="col fw-normal">DARKMODE.js</h1>
+      <p>learn more about the darkmode i used...</p>
+      <a href="https://darkmodejs.learn.uno/" rel="noopener noreferrer" target="_blank">https://darkmodejs.learn.uno/</a>
+
+      <h4>Notes: very simple to use, but think that this method does perhaps restricted</h4>
+
+
+      <br></br><br></br><br></br>
+      <h4>Demo sites</h4>
+      <p><a href="https://tradivegan.com" rel="noopener noreferrer" target="_blank">https://tradivegan.com (with custom label)</a></p>
+      <p><a href="https://what.toeat.in" rel="noopener noreferrer" target="_blank">https://what.toeat.in (without custom label)</a></p>
+      <p><a href="https://www.kanbanote.com" rel="noopener noreferrer" target="_blank">https://www.kanbanote.com (without the widget, once logged in)</a></p>
+      <p><a href="https://www.sandoche.com" rel="noopener noreferrer" target="_blank">https://www.sandoche.com (with custom label)</a></p>
+      <p><a href="https://pilab.dev" rel="noopener noreferrer" target="_blank">https://pilab.dev (without the widget, with a custom toggle button)</a></p>
+      
+    </div>
+  )
+}
+export default About
